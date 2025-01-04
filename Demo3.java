afc7a78a414c7dd7b917e1ced70799fd44e52e98
saicharan838 <saicharannaragani@gmail.com>
2025-01-04 12:33:23 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,17 @@
+public class Demo3 {
+    public static void main (String args[]){
+        int x = 10;
+        int y = 20;
+        int z = 30;
+
+
+        if(x>y){
+            System.out.println(x);
+        }else if(y>z){
+            System.out.println(y);
+        }else{
+            System.out.println(z);
+        }
+    }
+    
+}
