fda662e5c0b61898e773bdd99f3dc53f42177d93
saicharan838 <saicharannaragani@gmail.com>
2025-02-01 10:22:24 +0530

    Username And Password

---
+++ Demo005.java
@@ -0,0 +1,12 @@
+public class Demo005 {
+    public static void main(String[] args) {
+    String name = "saicharan";
+    String password = "sai_09";
+
+    while(name == "saicharan"  && password == "sai_09"){
+        System.out.println("malla reddy university");
+        break;
+    }
+    }
+    
+}
