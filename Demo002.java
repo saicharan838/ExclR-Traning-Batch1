263a958fcac83ca02a074a58f52a8fcf2f1eec77
saicharan838 <saicharannaragani@gmail.com>
2025-02-01 10:19:24 +0530

    Greatest Number b/w Two Digits

---
+++ Demo002.java
@@ -0,0 +1,16 @@
+public class Demo002 {
+    public static void main (String args[]){
+        
+         int x = 9;
+         int y = 10;
+
+
+        if(x>y){
+            System.out.println(x);
+        }else{
+            System.out.println(y);
+        }
+        
+    }
+    
+}
